220454a2385529a8afb79423dab37d8a964875e1
Luis <50057252+luisperez1050@users.noreply.github.com>
2022-06-04 07:31:05 -0400

    removed layer zero clean up

--- routes.js
+++
@@ -1,5 +0,0 @@
-// This file was automatically added by layer0 deploy.
-// You should commit this file to source control.
-import { Router } from '@layer0/core'
-
-export default new Router().fallback(({ renderWithApp }) => renderWithApp())
